70247d3daf9f7eeb198fea719ea19f24288623f8
GiovanniValzano <valzano.giovanni@gmail.com>
2022-11-14 09:02:43 +0100

    create script

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const app = new Vue ({
+    //qui inserisco il collegamento all'ID
+    el: '#root',
+    //qui inserisco i dati
+    data: {
+        primaClasse: 'stile', //questa è la creazione della classe assegnata al css
+        secondaClasse: 'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U' //questa è l'immagine
+    }
+    
+})
